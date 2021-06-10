3f13da6a0902d9329c8a99c5950386a4c296de5b
bgarcia <bgarcia@tekniker.es>
2021-06-10 16:10:56 +0200

    Training still running... (For model testing only)

--- run_augmentation.sh
+++ run_augmentation.sh
@@ -1,6 +1,6 @@
 source ENV_AE_DEV/bin/activate
 export PYTHONPATH=$PYTHONPATH:$PWD
-python Scripts/run_preprocessing.py --config Configs/PS_AE_test.INI -v
+python Scripts/run_preprocessing.py --config Configs/surfInspAE.INI -v
 deactivate
 
 
